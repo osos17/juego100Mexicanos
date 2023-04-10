8b22949c7b509473d0c74db4fec21d190adb9e12
osos17 <charlytutub11@gmail.com>
2023-04-09 21:03:05 -0600

    Proyecto Final100

---
+++ js/preguntas.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿En que año salio la primera computadora?",
+    answer: "Entre 1943 y 1945",
+    options: [
+      "Entre 1890 y 1899",
+      "Entre 1943 y 1945",
+      "Entre 1990 y 1995",
+      "Entre 1880 y 1900"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Año en que salio el primer Mouse?",
+    answer: "1967",
+    options: [
+      "1990",
+      "1880",
+      "1955",
+      "1967"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿En que año salio el primer teclado?",
+    answer: "1846",
+    options: [
+      "1790",
+      "2002",
+      "1846",
+      "1901"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Quien creo la primera pagina web?",
+    answer: "Tim Bernes-Lee",
+    options: [
+      "Tim Bernes-Lee",
+      "Jhon Lennon",
+      "Thomas Alba Edison",
+      "Michael Jackson"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuando se vendio por primera vez una computadora de escritorio?",
+    answer: "En 1964",
+    options: [
+      "En 1990",
+      "En 1940",
+      "En 1944",
+      "En 1964"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Tipos de Tecnologias",
+    answer: "Tecnologias duras y blandas",
+    options: [
+      "Tecnologias EIA y IA",
+      "Tecnologias duras y blandas",
+      "Tecnologias Fisicas y Logicas",
+      "Todas las anteriores"
+    ]
+  },
+  {
+    numb: 7,
+    question: "¿Quien invento el internet?",
+    answer: "Sir Tim Berners-Lee",
+    options: [
+      "Bad Bunny",
+      "Sir Tim Berners-Lee",
+      "Elon Musk",
+      "Jackie Chan"
+    ]
+  },
+  {
+    numb: 8,
+    question: "¿Cual es la funcion de un modem?",
+    answer: "Envia informacion  entre el mundo exterior o red de area (WAN)",
+    options: [
+      "Consiste en datos para redes sociales",
+      "Hace envio de paquetes de datos",
+      "Envia informacion  entre el mundo exterior o red de area (WAN)",
+      "Es independiente a su provedor de linea"
+    ]
+  },
+  {
+    numb: 9,
+    question: "¿Como se llama el cable de Internet?",
+    answer: "ETHERNET",
+    options: [
+      "USB",
+      "COAXIAL",
+      "MICRO SD",
+      "ETHERNET"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Editor para crear paginas web",
+    answer: "Microsoft Visual Studio",
+    options: [
+      "Microsoft Visual Studio",
+      "NetBeans",
+      "PHP",
+      "YouTube"
+    ]
+  },
+  
+];
